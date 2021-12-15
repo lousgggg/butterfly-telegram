197460321fa9fef20c470faad1ee87a12f89aa8f
= <=>
2021-12-15 17:43:59 +0800

    add ThreadPoolConfig

---
+++ src/main/java/com/luo/wiley/springbootdemo/configuration/ThreadPoolConfig.java
@@ -0,0 +1,25 @@
+package com.luo.wiley.springbootdemo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class ThreadPoolConfig {
+
+    @Bean("taskExecutor")
+    public Executor getTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(200);
+        executor.setKeepAliveSeconds(30);
+        executor.setThreadNamePrefix("test-task-executor-");
+
+        return executor;
+    }
+}
